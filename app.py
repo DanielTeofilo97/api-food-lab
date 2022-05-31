b95d3bf222a52b3a6e5977b2e4bf4d03bd88cd3e
Daniel Teófilo <50593453+DanielTeofilo97@users.noreply.github.com>
2022-05-31 16:50:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/validate-fish', methods=['GET'])
+@app.route('/healthcheck', methods=['GET'])
 def healthcheck():
     return jsonify(
                status=200,
